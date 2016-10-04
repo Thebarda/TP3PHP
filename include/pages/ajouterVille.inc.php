084b128d869510901a4bfcb30ec211543a73acfd
antoineDu87 <antoine.aymard.87@gmail.com>
2016-10-04 11:06:09 +0200

    debut ajout ville

--- include/pages/ajouterVille.inc.php
+++ include/pages/ajouterVille.inc.php
@@ -1,1 +1,27 @@
 <h1>Ajouter une ville</h1>
+
+
+<?php
+  private $nbVille;
+
+  if(!isset($_POST["vil_nom"])
+  {
+    ?>
+    <form method="POST" action="ex2.php">
+
+    nom : <input type="textarea" name="vil_nom" >
+    <input type="submit" value="envoyer">
+
+  </form>
+<?php
+  }
+  else
+  {
+
+    echo "la ville ".$_POST["vil_nom"]." a ete ajoutée ";
+
+
+  }
+
+
+ ?>
