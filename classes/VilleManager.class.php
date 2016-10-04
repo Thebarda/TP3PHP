be80f587bfca2bed14b6f0dbeb81cdf0615151a2
Thebarda <tomdar87@outlook.com>
2016-10-04 11:05:49 +0200

    ajout nbVilles et getAll

--- classes/VilleManager.class.php
+++ classes/VilleManager.class.php
@@ -1,4 +1,28 @@
 <?php
 class VilleManager{
-	
+	protected $db;
+	public function __construct($db){
+		$this->db = $db;
+	}
+
+	public function getAll(){
+		$listVilles = array();
+		$sql='SELECT vil_num, vil_nom FROM ville';
+		$req= $this->db->query($sql);
+		while($ville = $req->fetch(PDO::FETCH_OBJ)){
+			$listVilles[] = new Ville($ville);
+		}
+		$req->closeCursor();
+		return $listVilles;
+	}
+
+	public function getNbVille(){
+		$sql='SELECT COUNT(vil_num) AS nbVille, FROM ville';
+		$req=$this->db->query($sql);
+		$resu = $req->fetch(PDO::FETCH_OBJ);
+		if($resu != NULL){
+			return $resu->nbVille;
+		}
+	}
 }
+?>
