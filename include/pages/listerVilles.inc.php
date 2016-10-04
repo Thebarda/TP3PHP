0863aaa24ef316280f59b574c1aa648ac1749a94
Thebarda <tomdar87@outlook.com>
2016-10-04 11:18:29 +0200

    ajout autload + config

--- include/pages/listerVilles.inc.php
+++ include/pages/listerVilles.inc.php
@@ -1,2 +1,17 @@
+<h1>Liste des villes </h1>
 <?php
-
+  $db = new MyPdo();
+  $managerVille = new VilleManager($db);
+  $nbVilles = $managerVille->getNbVille();
+  $listVilles = $managerVille->getAll();
+  echo "<p>Actuellement ".$nbVilles." villes sont enregistrées</p>\n";
+  echo "<table>\n";
+  echo "<tr><th>Numéro</th><th>Nom</th></tr>\n";
+  foreach($listVilles as $ville){
+    ?><tr><?php
+    echo "<td>".$ville->getVil_num()."</td>";
+    echo "<td>".$ville->getVil_nom()."</td>";
+     ?></tr><?php
+  }
+  echo "</table>\n";
+?>
--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
+require_once("include/config.inc.php");
+require_once("include/autoload.inc.php");
+require_once("include/header.inc.php");
 
-require_once("include/header.inc.php"); 
- 
 ?>
 <div id="corps">
 <?php
-require_once("include/menu.inc.php"); 
+require_once("include/menu.inc.php");
 require_once("include/texte.inc.php");
 ?>
 </div>
