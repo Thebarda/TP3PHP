85da531cb071ade2475db86a9c287fd976438c29
antoineDu87 <antoine.aymard.87@gmail.com>
2016-10-18 08:34:03 +0200

    Add files via upload

--- classes/SalarieManager.class.php
+++ classes/SalarieManager.class.php
@@ -1,17 +1,14 @@
 <?php
 class SalarieManager{
-	protected $db;
-	public function __construct($db){
-		$this->db = $db;
-	}
+  protected $db;
+  public function __construct($db){
+    $this->db = $db;
+  }
 
-	public function add($salarie)
-	{
-		$requete=$this->db->prepare('INSERT INTO personne(per_num, sal_telprof, fon_num) VALUES (:per_num, :sal_telprof, :fon_num);');
-    $requete->bindValue(':per_num',$salarie->getPer_num(), PDO::PARAM_INT);
-    $requete->bindValue(':sal_telprof',$salarie->getSal_telprof(), PDO::PARAM_STR);
-		$requete->bindValue(':fon_num',$salarie->getFon_num(), PDO::PARAM_INT);
-    $retour=$requete->execute();
-	}
+  public function supprSalarie($per_num)
+  {
+      $requete=$this->db->prepare('DELETE FROM salarie WHERE per_num=:per_num');
+      $requete->bindValue(':per_num',$per_num,PDO::PARAM_INT);
+      $retour=$requete->execute();
+  }
 }
-?>
